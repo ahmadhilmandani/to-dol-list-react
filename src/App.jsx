aaffa3c1a67eb3f8cad41ca848e2aae66732193a
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-04-14 10:42:57 +0700

    UPDATE adding list is done insya Allah

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import bgMobileDark from './assets/img/bgMobileDark.jpg'
+
+
 import { List } from './List'
+
+import bgMobileDark from './assets/img/bgMobileDark.jpg'
 import iconMoon from './assets/img/icon-moon.svg'
 import iconSun from './assets/img/icon-sun.svg'
 import './App.css'
 
 function App() {
   const [lightMode, setLightMode] = useState(false)
-  const [list, setList] = useState({
+  const [listName, setListName] = useState('')
+  const [list, setList] = useState(
+    [
 
-  })
-  const test = [
-    {
-      work: 'Belajar backend',
-      done: false
-    },
-    {
-      work: 'minta ttd dospem',
-      done: true,
-
-    }
-  ]
+    ]
+  )
+  const addList = (e) => {
+    e.preventDefault()
+    setList([...list, { work: listName }])
+  }
 
   return (
     <div className='w-full min-h-screen flex justify-center gap-5 bg-Cust-Very-Dark-Blue relative px-6'>
@@ -31,14 +28,16 @@ function App() {
         <header className='flex justify-between items-center'>
           <h1 className='text-white font-semibold text-3xl tracking-widest'>TODO</h1>
 
-          {lightMode == true ? <img src={iconSun} alt="" />  : <img src={iconMoon} alt='' />}
+          {lightMode == true ? <img src={iconSun} alt="" /> : <img src={iconMoon} alt='' />}
 
         </header>
         <main className='mt-5'>
-          <input type="text" className='w-full px-3 py-3 rounded-md bg-Cust-Bg-Color text-white' />
+          <form method="post" onSubmit={addList} id='form-list'>
+            <input type="text" className='w-full px-3 py-3 rounded-md bg-Cust-Bg-Color text-white' onChange={(e) => { setListName(e.target.value) }} />
+          </form>
           <div className='mt-5 rounded-md bg-Cust-Bg-Color'>
 
-            <List test={test} />
+            <List list={list} />
 
             <div className='flex justify-between px-3 py-3 text-Cust-Dark-Grayish-Blue text-[10px]'>
               <div>3 items left</div>
--- src/List.jsx
+++ src/List.jsx
@@ -5,17 +5,16 @@ export function List(props) {
     return (
         <>
             {
-                props.test.map((test, index) => {
+                props.list.map((list, index) => {
                     return (
                         <div className='px-3 py-3 lg:px-5 lg:py-4 flex justify-between items-center text-xs md:text-base text-white border-b border-b-Cust-Very-Dark-Grayish-Blue' id={index}>
                             <div className='flex gap-3 items-center'>
-                                <div className={`w-[16px] h-[16px] lg:w-[20px] lg:h-[20px] rounded-full overflow-hidden flex justify-center items-center 
-                                 ${test.done ? 'bg-gradient-to-r from-Cust-Sky-Blue to-Cust-Purple' : 'border'}`} >
-                                    {test.done &&
-                                    <img src={iconCheck} alt="icon check" />
-                                }
+                                <div className={`w-[16px] h-[16px] lg:w-[20px] lg:h-[20px] rounded-full overflow-hidden flex justify-center items-center ${list.done ? 'bg-gradient-to-r from-Cust-Sky-Blue to-Cust-Purple' : 'border'}`} >
+                                    {list.done &&
+                                        <img src={iconCheck} alt="icon check" />
+                                    }
                                 </div>
-                                <p className={test.done == true ? 'line-through text-Cust-Dark-Grayish-Blue' : ''} >{test.work}</p>
+                                <p className={list.done == true ? 'line-through text-Cust-Dark-Grayish-Blue' : ''} >{list.work}</p>
                             </div>
                             <img src={iconX} alt="" className='w-[12px] h-[12px] lg:w-[16px] lg:h-[16px]' />
                         </div>
