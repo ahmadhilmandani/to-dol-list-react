25a62f4bcefff0f5abefd2c8b411aa0354a7aa1d
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-04-17 07:23:46 +0700

    UPDATE filter category done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { List } from './List'
 
 import bgMobileDark from './assets/img/bgMobileDark.jpg'
+import bgDesktopDark from './assets/img/bg-desktop-dark.jpg'
 import iconMoon from './assets/img/icon-moon.svg'
 import iconSun from './assets/img/icon-sun.svg'
 import './App.css'
@@ -17,6 +18,7 @@ function App() {
     ]
   )
   const [counter, setCounter] = useState(0)
+  const [category, setCategory] = useState('all')
 
   const addList = (e) => {
     e.preventDefault()
@@ -26,9 +28,10 @@ function App() {
 
 
   return (
-    <div className='w-full min-h-screen flex justify-center gap-5 bg-Cust-Very-Dark-Blue relative px-6'>
-      <div className='w-full h-52 bg-cover bg-no-repeat absolute' style={{ backgroundImage: `url(${bgMobileDark})` }}></div>
-      <div className='lg:w-full max-w-[480px] w-[90%] mt-20 absolute'>
+    <div className='w-full min-h-screen flex justify-center gap-5  relative px-6'>
+      <div className='lg:hidden block w-full h-52 bg-cover bg-no-repeat absolute z-[-2]' style={{ backgroundImage: `url(${bgMobileDark})` }}></div>
+      <div className='lg:block hidden w-full h-60 bg-cover bg-no-repeat absolute z-[-2]' style={{ backgroundImage: `url(${bgDesktopDark})` }}></div>
+      <div className='lg:w-full max-w-[480px] w-[90%] mt-20 lg:mt-24 mb-10 lg:mb-16'>
         <header className='flex justify-between items-center'>
           <h1 className='text-white font-semibold text-3xl tracking-widest'>TODO</h1>
           {lightMode == true ? <img src={iconSun} alt="" /> : <img src={iconMoon} alt='' />}
@@ -44,6 +47,7 @@ function App() {
           <div className='mt-5 rounded-md bg-Cust-Bg-Color'>
 
             <List list={list}
+              category = {category}
               showDone={(paramIndex) => {
                 const newArrList = list.map((list, index) => {
                   if (index == paramIndex && list.done != true) {
@@ -74,10 +78,25 @@ function App() {
 
             <div className='flex justify-between px-3 py-3 text-Cust-Dark-Grayish-Blue text-[10px]'>
               <div>{counter} items left</div>
-              <div>All</div>
-              <div>Active</div>
-              <div>Complete</div>
-              <div>Clear Complete</div>
+              <a onClick={(e)=>{
+                e.preventDefault()
+                setCategory('all')
+              }}>All</a>
+              <a onClick={(e)=>{
+                e.preventDefault()
+                setCategory('active')
+              }}>Active</a>
+              <a onClick={(e)=>{
+                e.preventDefault()
+                setCategory('complete')
+              }}>Complete</a>
+              <a onClick={(e)=>{
+                e.preventDefault()
+                setCategory('all')
+                setList([])
+                setListName('')
+                setCounter(0)
+              }}>Clear Complete</a>
             </div>
           </div>
         </main>
--- src/List.jsx
+++ src/List.jsx
@@ -6,27 +6,75 @@ export function List(props) {
         <>
             {
                 props.list.map((list, index) => {
-                    return (
-                        <div className='px-3 py-3 lg:px-5 lg:py-4 flex justify-between items-center text-xs md:text-base text-white border-b border-b-Cust-Very-Dark-Grayish-Blue' key={index}>
-                            <div className='flex gap-3 items-center'>
-                                <div className={`w-[16px] h-[16px] lg:w-[24px] lg:h-[24px] rounded-full overflow-hidden flex justify-center items-center ${list.done ? 'bg-gradient-to-r from-Cust-Sky-Blue to-Cust-Purple' : 'border hover:translate-y-[-5px] transition ease-in-out duration-100'}`}
-                                    onClick={() => {
-                                        props.showDone(index)
-                                    }
-                                    }
-                                >
-                                    {list.done &&
-                                        <img src={iconCheck} alt="icon check" />
-                                    }
+                    if (props.category == 'all') {
+                        return (
+                            <div className='px-3 py-3 lg:px-5 lg:py-4 flex justify-between items-center text-xs md:text-base text-white border-b border-b-Cust-Very-Dark-Grayish-Blue' key={index}>
+                                <div className='flex gap-3 items-center'>
+                                    <div className={`w-[16px] h-[16px] lg:w-[24px] lg:h-[24px] rounded-full overflow-hidden flex justify-center items-center ${list.done ? 'bg-gradient-to-r from-Cust-Sky-Blue to-Cust-Purple' : 'border hover:translate-y-[-5px] cursor-pointer transition ease-in-out duration-100'}`}
+                                        onClick={() => {
+                                            props.showDone(index)
+                                        }
+                                        }
+                                    >
+                                        {list.done &&
+                                            <img src={iconCheck} alt="icon check" />
+                                        }
+                                    </div>
+                                    <p className={list.done == true ? 'line-through text-Cust-Dark-Grayish-Blue' : ''} >{list.work}</p>
                                 </div>
-                                <p className={list.done == true ? 'line-through text-Cust-Dark-Grayish-Blue' : ''} >{list.work}</p>
+                                <img src={iconX} alt="" className='w-[12px] h-[12px] lg:w-[16px] lg:h-[16px] cursor-pointer'
+                                    onClick={() => {
+                                        props.removeList(index)
+                                    }} />
                             </div>
-                            <img src={iconX} alt="" className='w-[12px] h-[12px] lg:w-[16px] lg:h-[16px]'
-                                onClick={() => {
-                                    props.removeList(index)
-                                }} />
-                        </div>
-                    )
+                        )
+                    }
+                    else if (props.category == 'active' && list.done != true) {
+                        return (
+                            <div className='px-3 py-3 lg:px-5 lg:py-4 flex justify-between items-center text-xs md:text-base text-white border-b border-b-Cust-Very-Dark-Grayish-Blue' key={index}>
+                                <div className='flex gap-3 items-center'>
+                                    <div className={`w-[16px] h-[16px] lg:w-[24px] lg:h-[24px] rounded-full overflow-hidden flex justify-center items-center ${list.done ? 'bg-gradient-to-r from-Cust-Sky-Blue to-Cust-Purple' : 'border hover:translate-y-[-5px] cursor-pointer transition ease-in-out duration-100'}`}
+                                        onClick={() => {
+                                            props.showDone(index)
+                                        }
+                                        }
+                                    >
+                                        {list.done &&
+                                            <img src={iconCheck} alt="icon check" />
+                                        }
+                                    </div>
+                                    <p className={list.done == true ? 'line-through text-Cust-Dark-Grayish-Blue' : ''} >{list.work}</p>
+                                </div>
+                                <img src={iconX} alt="" className='w-[12px] h-[12px] lg:w-[16px] lg:h-[16px] cursor-pointer'
+                                    onClick={() => {
+                                        props.removeList(index)
+                                    }} />
+                            </div>
+                        )
+                    }
+                    else if (props.category == 'complete' && list.done == true) {
+                        return (
+                            <div className='px-3 py-3 lg:px-5 lg:py-4 flex justify-between items-center text-xs md:text-base text-white border-b border-b-Cust-Very-Dark-Grayish-Blue' key={index}>
+                                <div className='flex gap-3 items-center'>
+                                    <div className={`w-[16px] h-[16px] lg:w-[24px] lg:h-[24px] rounded-full overflow-hidden flex justify-center items-center ${list.done ? 'bg-gradient-to-r from-Cust-Sky-Blue to-Cust-Purple' : 'border hover:translate-y-[-5px] cursor-pointer transition ease-in-out duration-100'}`}
+                                        onClick={() => {
+                                            props.showDone(index)
+                                        }
+                                        }
+                                    >
+                                        {list.done &&
+                                            <img src={iconCheck} alt="icon check" />
+                                        }
+                                    </div>
+                                    <p className={list.done == true ? 'line-through text-Cust-Dark-Grayish-Blue' : ''} >{list.work}</p>
+                                </div>
+                                <img src={iconX} alt="" className='w-[12px] h-[12px] lg:w-[16px] lg:h-[16px] cursor-pointer'
+                                    onClick={() => {
+                                        props.removeList(index)
+                                    }} />
+                            </div>
+                        )
+                    }
                 })
             }
         </>
